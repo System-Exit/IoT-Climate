cb7f6242927dcf140963e1b4b492e998997ddf41
rmit-s3661720-daniel-linford <s3661720@student.rmit.edu.au>
2019-03-25 15:10:19 +1100

    Created Bluetooth class framework.

---
+++ bluetooth.py
@@ -0,0 +1,28 @@
+import bluetooth
+
+
+# Bluetooth notification class
+class BluetoothNotifier:
+    # Initialization
+    def __init__(self):
+        pass
+
+    # Connects to device
+    def connectToDevice(self):
+        pass
+
+    # Sends notification
+    def sendMessage(self):
+        pass
+
+    # Gets current temperature
+    def getTemp(self):
+        pass
+
+    # Gets current humidity
+    def getHumid(self):
+        pass
+
+# Main method for script
+if __name__ == "__main__":
+    pass
