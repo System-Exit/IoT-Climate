9b4f416014f0114ffbc28c823641acab033d5eb5
Volkor The Barbarian Warrior <volkor@thecum.zone>
2019-04-06 04:01:38 +1100

    add documentation for task d

---
+++ taskd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# TODO: This entire thing :p
+# https://plot.ly/python/
+# https://matplotlib.org/examples/api/barchart_demo.html
+# https://github.com/gak/pygooglechart
+# https://github.com/Dfenestrator/GooPyCharts
+# https://stackoverflow.com/questions/915940/python-plotting-libraries
+
+# I think we should have two line graphs, one showing the average temperature
+# (15 minute - hour average) and one of the humidity. They are simple, and
+# effective for long term weather data we're displaying.
